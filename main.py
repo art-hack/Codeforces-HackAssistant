ec3289494840acc76a431c8e12f788d79ec00574
Art-hack <17ucs036@lnmiit.ac.in>
2020-03-23 01:10:31 +0530

    Added Functionality for Text Case Input

--- main.py
+++ main.py
@@ -10,19 +10,23 @@ import shutil
 mainwin = Tk()
 mainwin.title('Codeforces Hack Assistant')
 
-Label(mainwin, text="Your Solution").grid(row=1, column=0,columnspan=15)
-Label(mainwin, text="Solution to hack").grid(row=1, column=16,columnspan=15)
+Label(mainwin, text="Your Solution (CPP)").grid(row=1, column=0,columnspan=15)
+Label(mainwin, text="Solution to hack (CPP)").grid(row=1, column=16,columnspan=15)
 
 source = ScrolledText(mainwin, height=10, width=60)
 source.grid(row=2, column=0,padx=20, pady=(0,50),columnspan=15)
 compare = ScrolledText(mainwin, height=10, width=60)
 compare.grid(row=2, column=16,padx=20, pady=(0,50),columnspan=15)
-Label(mainwin, text="Test Case Generator").grid(row=3, column=0,columnspan=15)
+Label(mainwin, text="Test Case Generator (PYTHON)").grid(row=3, column=0,columnspan=15)
 generator = ScrolledText(mainwin, height=10, width=60)
-generator.grid(row=4, rowspan=2,column=0,padx=20, pady=(0,50),columnspan=15)
+generator.grid(row=4, rowspan=3,column=0,padx=20, pady=(0,50),columnspan=15)
 Label(mainwin, text="Number of Threads: ").grid(row=4, column=17,columnspan=5)
 spin = Spinbox(mainwin, from_= 1, to = 16)
 spin.grid(row=4,column = 22)
+var1 = tk.IntVar()
+checkbox = tk.Checkbutton(mainwin, text='Use Test Case Instead',variable=var1, onvalue=1, offvalue=0)
+checkbox.grid(row=5,column=22)
+
 tex=""
 execute = ""
 exit=""
@@ -68,6 +72,7 @@ def exit2():
         printf("Closed Worker: "+str(i+1))
     workers.clear()
     tex.configure(state='normal')
+    checkbox.configure(state='normal')
     exit.configure(state='disabled')
     source.configure(state='normal')
     execute.configure(state='normal')
@@ -79,7 +84,7 @@ def exit2():
     sys.exit()
 
 
-def exit():
+def exitt():
     p = threading.Thread(target=exit2, name='exit2')
     p.start()
 
@@ -119,29 +124,35 @@ def compares(input,pp):
         return False
     if(a[0]!=b[0]):
         printf("Output Mismatch on the following input!\n"+str(input)+"\nExiting..")
-        continue_executing=False
+        exitt()
         return False
     return True
 
 
-def run(pp):
+def run(pp,single=False):
     global continue_executing, available
     available.acquire()
+    if(continue_executing==False):
+        available.release()
+        return
     p = sub.Popen('g++ usable/source.cpp -o usable/source'+pp,stdout=sub.PIPE,stderr=sub.PIPE)
     output, errors = p.communicate()
     errors = errors.decode('ASCII')
     if(errors!=""):
-        continue_executing = False
+        exitt()
         printf("Source Compilation Error!\nExiting..")
+        available.release()
         return
     p = sub.Popen('g++ usable/compare.cpp -o usable/compare'+pp,stdout=sub.PIPE,stderr=sub.PIPE)
     output, errors = p.communicate()
     errors = errors.decode('ASCII')
     if(errors!=""):
         printf("Solution to Hack Compilation Error!\nExiting..")
-        continue_executing = False
+        exitt()
+        available.release()
         return
-    rewrite("usable/generator"+pp+".py",generator.get(1.0, END))
+    if(not single):
+        rewrite("usable/generator"+pp+".py",generator.get(1.0, END))
     available.release()
 
     # p = sub.Popen('python generator.py',stdout=sub.PIPE,stderr=sub.PIPE)
@@ -154,9 +165,17 @@ def run(pp):
     #     return
 
     while(continue_executing):
+        if single:
+            result = compares(str(generator.get(1.0, END)),pp)
+            if(result):
+                passed()
+            exitt()
+            return
+
         generate=execut('generator'+pp,"","Python")
         if(generate[1]!=""):
-            print("Generator Error!\n"+generate[1]+"\nExiting..")
+            printf("Generator Error!\n"+generate[1]+"\nExiting..")
+            exitt()
             return
         result = compares(generate[0],pp)
         if(result):
@@ -166,33 +185,39 @@ def run(pp):
     # print(output,errors,output=='')
 
 
+
 def printer():
-    global source, compare, generator, execute, spin, tex, workers, exit
+    global source, compare, generator, execute, spin, tex, workers, exit, checkbox, var1
     tex = Text(mainwin,height=10, width=120)
-    tex.grid(row=6,padx=20,pady=(0,20), column=0,columnspan=30)
+    tex.grid(row=7,padx=20,pady=(0,20), column=0,columnspan=30)
     rewrite("usable/source.cpp",source.get(1.0, END))
     rewrite("usable/compare.cpp",compare.get(1.0, END))
-
     tex.configure(state='disabled')
+    checkbox.configure(state='disabled')
     exit.configure(state='normal')
     source.configure(state='disabled')
     execute.configure(state='disabled')
     spin.configure(state='disabled')
     compare.configure(state='disabled')
     generator.configure(state='disabled')
-    
-    for i in range(int(spin.get())):
-        p = threading.Thread(target=run, args=(str(i)),name=str(i))
+    if(var1.get()==1):
+        p = threading.Thread(target=run, args=(str(0),True),name=str(0))
         p.start()
         workers.append(p)
-        printf("Started Thread: "+str(i+1))
+        printf("Started Thread: 1")
+    else:    
+        for i in range(int(spin.get())):
+            p = threading.Thread(target=run, args=(str(i)),name=str(i))
+            p.start()
+            workers.append(p)
+            printf("Started Thread: "+str(i+1))
     return
 
 
 execute = Button(mainwin, text="Execute", command=printer)
-execute.grid(row=5, column=18, columnspan=5, sticky="EW",pady=(0,25))
-exit = Button(mainwin, text="Stop", command=exit)
-exit.grid(row=5, column=25, columnspan=3, sticky="EW",pady=(0,25))
+execute.grid(row=6, column=18, columnspan=5, sticky="EW",pady=(0,25))
+exit = Button(mainwin, text="Stop", command=exitt)
+exit.grid(row=6, column=25, columnspan=3, sticky="EW",pady=(0,25))
 exit.configure(state='disabled')
 mainwin.protocol("WM_DELETE_WINDOW", on_closing)
 mainwin.mainloop()
